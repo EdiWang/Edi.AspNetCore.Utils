5531d3d1e99c155d0e881c19eb9d37a2b23153f7
Edi Wang <Edi.Wang@outlook.com>
2025-09-19 10:14:35 +0800

    Create DomainDataHelper.cs

---
+++ src/Edi.AspNetCore.Utils/DomainDataHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Edi.AspNetCore.Utils;
+
+public static class DomainDataHelper
+{
+    public static void SetAppDomainData(string key, object value)
+    {
+        AppDomain.CurrentDomain.SetData(key, value);
+    }
+
+    public static T GetAppDomainData<T>(string key, T defaultValue = default(T))
+    {
+        object data = AppDomain.CurrentDomain.GetData(key);
+        if (data == null)
+        {
+            return defaultValue;
+        }
+
+        return (T)data;
+    }
+}
