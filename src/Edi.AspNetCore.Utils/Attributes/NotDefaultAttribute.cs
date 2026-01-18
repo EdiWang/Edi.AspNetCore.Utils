289ffb9d490992680b74acd7f78268db217a906f
Edi Wang <Edi.Wang@outlook.com>
2026-01-18 10:10:33 +0800

    Add XML documentation to validation attributes
    Added XML summary, remarks, and parameter documentation to NotDefaultAttribute and NotEmptyAttribute classes to improve code clarity and IntelliSense support. These comments explain the purpose, usage, and behavior of the validation attributes.

--- src/Edi.AspNetCore.Utils/Attributes/NotDefaultAttribute.cs
+++ src/Edi.AspNetCore.Utils/Attributes/NotDefaultAttribute.cs
@@ -2,11 +2,35 @@
 
 namespace Edi.AspNetCore.Utils.Attributes;
 
+/// <summary>
+/// Validation attribute that ensures a property value is not the default value for its type.
+/// For value types, validates that the value is not the default (e.g., 0 for int, false for bool).
+/// For reference types, validates that the value is not null.
+/// </summary>
+/// <remarks>
+/// This attribute allows null values to be considered valid. If you need to prevent null values,
+/// combine this attribute with the <see cref="RequiredAttribute"/>.
+/// </remarks>
 public class NotDefaultAttribute : ValidationAttribute
 {
+    /// <summary>
+    /// The default error message displayed when validation fails.
+    /// </summary>
     public const string DefaultErrorMessage = "The {0} field must not have the default value";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="NotDefaultAttribute"/> class.
+    /// </summary>
     public NotDefaultAttribute() : base(DefaultErrorMessage) { }
 
+    /// <summary>
+    /// Validates that the specified value is not the default value for its type.
+    /// </summary>
+    /// <param name="value">The value to validate.</param>
+    /// <returns>
+    /// <see langword="true"/> if the value is null, or if it's a value type that is not equal to its default value,
+    /// or if it's a non-null reference type; otherwise, <see langword="false"/>.
+    /// </returns>
     public override bool IsValid(object value)
     {
         //NotDefault doesn't necessarily mean required
--- src/Edi.AspNetCore.Utils/Attributes/NotEmptyAttribute.cs
+++ src/Edi.AspNetCore.Utils/Attributes/NotEmptyAttribute.cs
@@ -2,12 +2,30 @@
 
 namespace Edi.AspNetCore.Utils.Attributes;
 
+/// <summary>
+/// Validates that a value is not empty. Currently supports <see cref="Guid"/> validation.
+/// </summary>
+/// <remarks>
+/// This attribute does not enforce that a value is required. Null values are considered valid.
+/// For <see cref="Guid"/> types, the value must not be <see cref="Guid.Empty"/>.
+/// </remarks>
 // https://andrewlock.net/creating-an-empty-guid-validation-attribute/
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
 public class NotEmptyAttribute() : ValidationAttribute(DefaultErrorMessage)
 {
+    /// <summary>
+    /// The default error message template used when validation fails.
+    /// </summary>
     public const string DefaultErrorMessage = "The {0} field must not be empty";
 
+    /// <summary>
+    /// Validates that the specified value is not empty.
+    /// </summary>
+    /// <param name="value">The value to validate.</param>
+    /// <returns>
+    /// <see langword="true"/> if the value is null, or if the value is a <see cref="Guid"/> 
+    /// that is not equal to <see cref="Guid.Empty"/>; otherwise, <see langword="false"/>.
+    /// </returns>
     public override bool IsValid(object value)
     {
         // NotEmpty doesn't necessarily mean required
